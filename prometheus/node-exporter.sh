ff8996f1e81da5dea573bd3b9a63c32efcef04f7
Parham Alvani <parham.alvani@gmail.com>
2019-05-31 00:16:38 +0430

    feat: Complete The Node Exporter Installer

--- prometheus/node-exporter.sh
+++ prometheus/node-exporter.sh
@@ -9,7 +9,17 @@
 # =======================================
 version="0.16.0"
 
+# based on https://devopscube.com/monitor-linux-servers-prometheus-node-exporter/
+
+# Downloads and Installs the node exporter
 curl -# -L "https://github.com/prometheus/node_exporter/releases/download/v${version}/node_exporter-${version}.linux-amd64.tar.gz" -o node_exporter.tar.gz
 tar xvfz node_exporter.tar.gz
 rm node_exporter.tar.gz
 chmod +x node_exporter-${version}.linux-amd64/node_exporter
+sudo mv node_exporter-${version}.linux-amd64/node_exporter /usr/local/bin
+
+# Creates a specific user for node_exporter
+sudo useradd -rs /bin/false node_exporter
+
+# Creates a node exporter service
+sudo cp node_exporter.service /etc/systemd/system/node_exporter.service
